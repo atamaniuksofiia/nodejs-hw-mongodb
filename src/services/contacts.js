10d34b845ed635fdb4c040962106c54db629308d
atamaniuksofiia <165192279+atamaniuksofiia@users.noreply.github.com>
2025-02-13 13:29:39 +0200

    remove try catch

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,74 +1,33 @@
 import Contact from '../models/contact.js';
 
 export const getAllContacts = async () => {
-  try {
-    const contacts = await Contact.find();
-    if (contacts.length === 0) {
-      console.log('No contacts found in the database.');
-    } else {
-      console.log('Contacts retrieved from DB:', contacts);
-    }
+  const contacts = await Contact.find();
 
-    const contactsFormatted = contacts.map((contact) => ({
-      ...contact.toObject(),
-      _id: contact._id.toString(),
-    }));
-
-    return contactsFormatted;
-  } catch (error) {
-    console.error('Error fetching contacts from DB:', error.message);
-    throw new Error('Error fetching contacts: ' + error.message);
-  }
+  return contacts;
 };
 
 export const getContactById = async (contactId) => {
-  try {
-    const contact = await Contact.findById(contactId);
-    if (!contact) {
-      throw new Error('Contact not found');
-    }
-    return contact;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  const contact = await Contact.findById(contactId);
+
+  return contact;
 };
 export const createContact = async (contactData) => {
-  try {
-    const newContact = new Contact(contactData);
-    await newContact.save();
-    return newContact;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  const newContact = await Contact.create(contactData);
+
+  return newContact;
 };
 
 export const updateContactById = async (contactId, updateData) => {
-  try {
-    const updatedContact = await Contact.findByIdAndUpdate(
-      contactId,
-      updateData,
-      { new: true, runValidators: true },
-    );
+  const updatedContact = await Contact.findByIdAndUpdate(
+    contactId,
+    updateData,
+    { new: true, runValidators: true },
+  );
 
-    if (!updatedContact) {
-      throw new Error('Contact not found');
-    }
-
-    return updatedContact;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  return updatedContact;
 };
 export const deleteContactById = async (contactId) => {
-  try {
-    const deletedContact = await Contact.findByIdAndDelete(contactId);
+  const deletedContact = await Contact.findByIdAndDelete(contactId);
 
-    if (!deletedContact) {
-      throw new Error('Contact not found');
-    }
-
-    return deletedContact;
-  } catch (error) {
-    throw new Error(error.message);
-  }
+  return deletedContact;
 };
